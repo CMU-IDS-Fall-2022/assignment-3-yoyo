53c0fe013c4e66537f6a7b9b83fd39dd607b9832
tcabezon <tcabezon@andrew.cmu.edu>
2022-10-12 11:40:04 -0400

    tomas changes

--- streamlit_app_tomas.py
+++ streamlit_app_tomas.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+
+from altair import Color,Scale
 from streamlit.components.v1 import html
+import numpy as np
 
 def get_slide_data_tomas(df,artists,albums):
     labels = pd.Series([1] * len(df), index=df.index)
@@ -22,25 +25,52 @@ def load_data():
 
 df = load_data()
 ourArtist=['Bad Bunny', 'Drake', 'Ed Sheeran', 'keshi', 'ROSALÍA'] #we are only interested in songs of this 5 artists
+df=df[df['artist'].isin(ourArtist)]
+
+chart__ = alt.Chart(df).mark_bar().encode(
+    Color('aritst:O',
+          scale=Scale(domain=['Bad Bunny', 'Drake', 'Ed Sheeran', 'keshi', 'ROSALÍA'],
+                      range=['black', 'gold','red','blue','pink'])),
+    x = "sum(playcount):Q",
+    y = "artist:O",
+)
+
+
 
 st.write("Let's look at raw data in the Pandas Data Frame.")
+
+print(df)
 
 st.write(df)
 
 st.write("Which are the top played songs of the most streamed artists on spotify? ")
 
-artist_chart=alt.Chart(df[df['artist'].isin(ourArtist)]).mark_bar().encode(
-    x='sum(playcount)',
-    y=alt.Y('artist',sort='-x'),
-    color='artist'
-)
-st.altair_chart(artist_chart)
+# selection
+artist_brush = alt.selection_multi(fields = ["artist"])
+
+
+#selection.multi
+artist_chart = alt.Chart(df).mark_bar().encode(
+    x = "sum(playcount)",
+    y = alt.Y("artist",sort='-x'),
+    color ='artist'
+).add_selection(artist_brush)
+
+album_chart = alt.Chart(df).mark_bar().encode(
+    x = "count()",
+    y = alt.Y("album"),
+    color = 'artist'
+).transform_filter(artist_brush)
+
+st.write(artist_chart&album_chart)
+
+
 # st.write("total songs",df['artist'].count())
 cols=st.columns(2)
 
-us=['Drake','keshi']
+
 with cols[0]:
-    artists=st.multiselect('artist',df[df['artist'].isin(ourArtist)]['artist'].unique())
+    artists=st.multiselect('artist',df[df['artist'].isin(ourArtist)]['artist'].unique(),default=["ROSALÍA"]) # we add a default so we can plot an initial chart
 with cols[1]:
     albums=st.multiselect('album',df[df['artist'].isin(artists)]['album'].unique())
 
@@ -50,7 +80,7 @@ slice_labels=get_slide_data_tomas(df,artists,albums)
 song_count=st.slider('number_songs',
                     min_value=0,
                     max_value=len(slice_labels[slice_labels==1]),
-                    value=(len(slice_labels[slice_labels==1])))
+                    value=10)
 
 st.write("The sliced dataset contains {} elements".format(slice_labels.sum()))
 
@@ -60,13 +90,26 @@ song_playcount=ordered_dataset['playcount'].mean()
 st.metric('Mean listeners per song',song_playcount)
 # st.write(ordered_dataset)
 
-chart=alt.Chart(ordered_dataset).mark_bar().encode(
+chart1=alt.Chart(ordered_dataset).mark_bar().encode(
     x='sum(playcount)',
     y=alt.Y('name',sort='-x'),
     color='artist'
 )
 
-st.altair_chart(chart)
+chart2=alt.Chart(ordered_dataset).mark_bar().encode(
+    x='sum(playcount)',
+    y=alt.Y('album',sort='-x'),
+    color='artist'
+)
+
+cols_=st.columns(2)
+
+
+with cols_[0]:
+    st.altair_chart(chart1)
+with cols_[1]:
+    st.altair_chart(chart2)
+
 
 
 # chart = alt.Chart(df).mark_point().encode(
@@ -80,8 +123,11 @@ st.altair_chart(chart)
 # st.write(chart)
 
 st.markdown("This project was created by Student1 and Student2 for the [Interactive Data Science](https://dig.cmu.edu/ids2022) course at [Carnegie Mellon University](https://www.cmu.edu).")
-
-
+countValues=ordered_dataset.groupby('artist')['playcount'].sum()
+print(countValues.to_numpy())
+print(type(countValues.to_numpy()))
+st.write(countValues.to_numpy())
+# st.write(ordered_dataset)
 # Define your javascript
 my_js = """
 alert("Hola mundo");
